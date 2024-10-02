ca4c2b7a514b4d32ba2fd264f4cb111e2b074268
Hans Larsen <hans@larsen.online>
2024-10-02 12:27:52 -0700

    it works

--- src/nfc-tester/src/main.rs
+++ src/nfc-tester/src/main.rs
@@ -1,21 +1,46 @@
-extern crate nfc;
+use std::time::Duration;
 
-use nfc::context;
-use nfc::misc;
-use std::ptr::{null, null_mut};
+fn main() -> nfc1::Result<()> {
+    println!("libnfc v{}", nfc1::version());
 
-fn main() {
-    let mut context = context::new();
+    let mut context = nfc1::Context::new()?;
+    let mut device = context.open_with_connstring("pn532_uart:/dev/ttyUSB0")?;
 
-    if context.is_null() {
-        println!("Unable to initialize new NFC context!");
-    }
+    println!(
+        "NFC device {:?} opened through connection {:?}",
+        device.name(),
+        device.connstring()
+    );
+    println!(
+        "- Initiator modulations: {:?}",
+        device.get_supported_modulation(nfc1::Mode::Initiator)?
+    );
+    println!(
+        "- Target modulations: {:?}",
+        device.get_supported_modulation(nfc1::Mode::Target)?
+    );
 
-    // Initialize libnfc
-    nfc::init(&mut context);
+    device.initiator_init()?;
+    println!("polling");
 
-    // Print libnfc version
-    println!("libnfc version: {}", misc::version());
-    let devices = nfc::list_devices(context, null_mut(), 0);
-    eprintln!("Found {} NFC device(s)", devices);
+    // polling forever
+        let result = device.initiator_poll_target(&[
+            nfc1::Modulation {
+                modulation_type: nfc1::ModulationType::Iso14443a,
+                baud_rate: nfc1::BaudRate::Baud106,
+            },
+            nfc1::Modulation {
+                modulation_type: nfc1::ModulationType::Iso14443b,
+                baud_rate: nfc1::BaudRate::Baud106,
+            },
+            nfc1::Modulation {
+                modulation_type: nfc1::ModulationType::Felica,
+                baud_rate: nfc1::BaudRate::Baud212,
+            },
+
+        ], 20, Duration::from_millis(450));
+
+        println!("{:?}", result);
+
+    Ok(())
 }
